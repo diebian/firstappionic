1334272d053012b427b4b4b4c2a6fa90278f9980
Diego Mamani Zanca <dmz3010@gmail.com>
2019-01-04 09:30:51 -0400

    config project

---
+++ frontend/src/app/services/lista-deseos.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+
+export class ListaDeseosService {
+
+    constructor() {
+
+        console.log('Servicio inicializado');
+
+    }
+}
